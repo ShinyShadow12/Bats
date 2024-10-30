96d100c603ba2b236b03309144a4107253aac75a
ShinyShadow_ <83542827+ShinyShadow12@users.noreply.github.com>
2024-10-30 09:32:29 +0100

    Add files via upload

--- ConnectFTP(Upload&Download).bat
+++ ConnectFTP(Upload&Download).bat
@@ -1,6 +1,7 @@
 echo off
-
-echo open 134.0.11.185>>FTPCommands.txt
+:START
+set con=2
+echo open 134.0.11.185>FTPCommands.txt
 
 echo List of available users:
 echo.
@@ -12,6 +13,7 @@ echo.
 set /p UserChosen=Choose the user you want to login with: 
 if %UserChosen%==1 echo pruebas129>>FTPCommands.txt && echo )42gXJxd>>FTPCommands.txt
 
+:ACTION
 echo -------------------
 echo ( 1 ) - Download
 echo ( 2 ) - Upload
@@ -32,28 +34,31 @@ echo -------------------
 echo ( 1 ) - Test_1.txt
 echo ( 2 ) - Test_2.txt
 echo ( 3 ) - Test_3.txt
+echo ( 4 ) - Show all the available files
 echo -------------------
 echo.
-set /p FileChosen=Choose the file you want to download: 
+set /p FileChosen=Choose one of the options to download or write the name of the file with the extension: 
 
+if %FileChosen%==4 GOTO SHOWALL
+if NOT %FileChosen%==1 if NOT %FileChosen%==2 if NOT %FileChosen%==3 echo get %FileChosen%>>FTPCommands.txt && set con=1
+if NOT %con%==1 echo get Test_%FileChosen%.txt>>FTPCommands.txt
 
-echo get Test_%FileChosen%.txt>>FTPCommands.txt
 
 echo bye>>FTPCommands.txt
 
 ftp -s:FTPCommands.txt -v
 del FTPCommands.txt
-del *.tmp
-exit
+GOTO END
 
 :UPLOAD
 
 set /p UserFile=Write the name or absolute path of the file:
 
 echo put %UserFile%.txt >> FTPCommands.txt
+echo bye >> FTPCommands.txt
 ftp -s:FTPCommands.txt -v
 del FTPCommands.txt
-
+GOTO END
 
 
 :CREATE
@@ -75,7 +80,23 @@ echo put %FileName%.txt>>FTPCommands.txt
 echo bye>>FTPCommands.txt
 ftp -s:FTPCommands.txt -v
 del FTPCommands.txt
+GOTO END
 
-echo File uploaded successfully
+
+:SHOWALL
+
+echo dir >> FTPCommands.txt
+echo bye >> FTPCommands.txt
+ftp -s:FTPCommands.txt -v
+echo open 134.0.11.185>FTPCommands.txt
+echo pruebas129>>FTPCommands.txt
+echo )42gXJxd>>FTPCommands.txt
+
+GOTO ACTION
+
+:END
+set /p yesno=echo Action successful. Do you want to make another action? (S/N):
+
+if %yesno%==S GOTO START
 
 pause > nul
